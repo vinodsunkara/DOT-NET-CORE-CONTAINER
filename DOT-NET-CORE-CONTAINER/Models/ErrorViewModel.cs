57ab0f0f936f377ef8c9b3de78a24d548cf4e2aa
vinodsunkara <37222937+vinodsunkara@users.noreply.github.com>
2021-02-05 23:40:43 -0500

    Add project files.

---
+++ DOT-NET-CORE-CONTAINER/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DOT_NET_CORE_CONTAINER.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
